2ec0df7b435b84989d80774b2652b24c2de69f70
Charles Gian Esperanza <charlesgianesperanza@gmail.com>
2026-03-05 23:57:54 +0800

    third commit: transferred file paths to clean the main program

--- constants.py
+++ constants.py
@@ -1,13 +1,23 @@
 FILE_EXTENSIONS = {
-    "Images": [".jpg", ".jpeg", ".gif", ".png", ".webp", ".bmp", ".svg", ".cr2", ".nef", ".dng", ".heic",
+    "images": [".jpg", ".jpeg", ".gif", ".png", ".webp", ".bmp", ".svg", ".cr2", ".nef", ".dng", ".heic",
                 ".heif", ".tif", ".tiff", ".cr3", ".nef", ".arw", ".avif"],
-    "Videos": [".mp4", ".mov", ".avi", ".mkv", ".wmv", ".m4v", ".mpg",".mpeg", ".mts", ".m2ts", ".webm",
+    "videos": [".mp4", ".mov", ".avi", ".mkv", ".wmv", ".m4v", ".mpg",".mpeg", ".mts", ".m2ts", ".webm",
                ".flv", ".mxf"],
-    "Audios": [".mp3", ".wav", ".flac", ".ogg", ".m4a", ".aiff", ".alac", ".aac", ".oga", ".mp4"]
+    "audios": [".mp3", ".wav", ".flac", ".ogg", ".m4a", ".aiff", ".alac", ".aac", ".oga", ".mp4"]
 }
 
 DIRECTORIES_TO_CLEAN = [
     "C:/Users/Cess/Desktop/File Converter/Photos/jpeg result",
     "C:/Users/Cess/Desktop/File Converter/Videos/MP4 result",
-    "C:/Users/Cess/Desktop/File Converter/Audios/MP3 result"
+    "C:/Users/Cess/Desktop/File Converter/Audios/MP3 result",
+    "D:/Programming Projects/Python/file_converter"
 ]
+
+FILE_PATHS = {
+    "img_not_converted": "C:/Users/Cess/Desktop/File Converter/Photos/Image converter",
+    "img_converted": "C:/Users/Cess/Desktop/File Converter/Photos/jpeg result",
+    "vid_not_converted": "C:/Users/Cess/Desktop/File Converter/Videos/Vid converter",
+    "vid_converted": "C:/Users/Cess/Desktop/File Converter/Videos/MP4 result",
+    "aud_not_converted": "C:/Users/Cess/Desktop/File Converter/Audios/Aud converter",
+    "aud_converted": "C:/Users/Cess/Desktop/File Converter/Audios/MP3 result"
+}
