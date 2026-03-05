d11fee8bed1ba4786d312c2bc533fb8bd74f87ec
Charles Gian Esperanza <charlesgianesperanza@gmail.com>
2026-03-05 12:17:36 +0800

    second commit: created image converter function, not working

--- file_converter.py
+++ file_converter.py
@@ -18,7 +18,7 @@ aud_not_converted = "C:/Users/Cess/Desktop/File Converter/Audios/Aud converter"
 aud_converted = "C:/Users/Cess/Desktop/File Converter/Audios/MP3 result"
 
 class FileHandler(FileSystemEventHandler):
-    def file_detected(self, event):
+    def on_created(self, event):
         if event.is_directory:
             return
         file_path = event.src_path
@@ -35,8 +35,18 @@ class FileHandler(FileSystemEventHandler):
         elif file_type in [".wav", ".flac", ".ogg", ".m4a", ".aiff", ".alac", ".aac", ""]:
             self.aud_convert(file_path, file_name)
 
-    def image_convert(self, file_path, file_name):
+    def image_convert(self, file_path, name):
+        time.sleep(1)
+        converted_jpeg = os.path.join(img_converted, f"{name}.jpg")
+        with Image.open(file_path) as img:
+            img.convert("RGB").save(converted_jpeg)
+        print(f"Successfully converted {name} to JPEG")
 
-    def vid_convert(self, file_path, file_name):
+if __name__ == "__main__":
+    event_handler = FileHandler()
+    observer = Observer()
 
-    def aud_convert(self, file_path, file_name):
+    observer.schedule(event_handler, img_not_converted, recursive=False)
+
+    observer.start()
+    print("Starting file converter")
