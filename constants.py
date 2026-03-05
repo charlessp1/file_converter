6dfff1e63ea14f6959bbb848cc276f7c854572f2
Charles Gian Esperanza <charlesgianesperanza@gmail.com>
2026-03-05 22:42:34 +0800

    initial commit: added file extensions and directories to clean

---
+++ constants.py
@@ -0,0 +1,13 @@
+FILE_EXTENSIONS = {
+    "Images": [".jpg", ".jpeg", ".gif", ".png", ".webp", ".bmp", ".svg", ".cr2", ".nef", ".dng", ".heic",
+                ".heif", ".tif", ".tiff", ".cr3", ".nef", ".arw", ".avif"],
+    "Videos": [".mp4", ".mov", ".avi", ".mkv", ".wmv", ".m4v", ".mpg",".mpeg", ".mts", ".m2ts", ".webm",
+               ".flv", ".mxf"],
+    "Audios": [".mp3", ".wav", ".flac", ".ogg", ".m4a", ".aiff", ".alac", ".aac", ".oga", ".mp4"]
+}
+
+DIRECTORIES_TO_CLEAN = [
+        "C:/Users/Cess/Desktop/File Converter/Photos/jpeg result",
+        "C:/Users/Cess/Desktop/File Converter/Videos/MP4 result",
+        "C:/Users/Cess/Desktop/File Converter/Audios/MP3 result"
+    ]
